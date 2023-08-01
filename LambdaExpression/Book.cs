c2731d54359c2c707195e57b3b4beab010e7dd26
rohibus <rohitsince1998@gmail.com>
2023-08-01 09:40:16 +0530

    Lambda Expressions

---
+++ LambdaExpression/Book.cs
@@ -0,0 +1,9 @@
+namespace LambdaExpression
+{
+    public class Book
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ LambdaExpression/BookRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace LambdaExpression
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "Title 1", Price = 5},
+                new Book() {Title = "Title 2", Price = 7},
+                new Book() {Title = "Title 3", Price = 17}
+            };
+        }
+    }
+}
