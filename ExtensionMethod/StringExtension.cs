e524e9b5d6816be55c0f4b8a51ea5011d535f5a6
rohibus <rohitsince1998@gmail.com>
2023-08-02 08:25:04 +0530

    ExtensionMethod

---
+++ ExtensionMethod/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ExtensionMethod
+{
+	public static class StringExtension
+	{
+		public static string Shorten(this String str, int numberOfWords)
+		{
+			if (numberOfWords < 0)
+				throw new ArgumentOutOfRangeException("NumberofWords must be greater than or equal to zero");
+
+			if (numberOfWords == 0)
+				return "";
+
+			var words = str.Split(' ');
+
+			if (words.Length <= numberOfWords)
+				return str;
+
+			return String.Join(" ", words.Take(numberOfWords)) + "...";
+		}
+	}
+}
+
